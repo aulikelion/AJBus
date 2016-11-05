d9bdf3f7f9bbd3c93a1949212c76e2f40e8b9f61
jwurbane97 <jwurbane97@ajou.ac.kr>
2016-11-05 04:06:55 +0000

    yangjae added

--- app/controllers/station_controller.rb
+++ app/controllers/station_controller.rb
@@ -18,4 +18,22 @@ class StationController < ApplicationController
         @prevLocation1s = data.xpath("//locationNo1")
         @remainSeat = data.xpath("//remainSeatCnt1")
     end
+    
+    def huisu
+        station = Station.where(code: 121000971).first
+        @name = station.name
+        @bound = station.bound
+        
+        base_url = "http://openapi.gbis.go.kr/ws/rest/"
+        service = ["buslocationservice?serviceKey=", "busarrivalservice/station?serviceKey=", "busarrivalservice?serviceKey="]
+        serviceKey = "yn8SpYJkbYBaQJV0SLIzuQgTelxHC%2FUABDIVXlYNKs3mNZy6s8aCsUwlX2FD7RbjZTQB1vtvsvaGNxaxlqTL1w%3D%3D&"
+    
+        url = base_url+service[1]+serviceKey+'stationId=121000971'
+        data = Nokogiri::XML(open(url))
+        @routeIds = data.xpath("//routeId")
+        @locationNo1s = data.xpath("//predictTime1")
+        @locationNo2s = data.xpath("//predictTime2")
+        @prevLocation1s = data.xpath("//locationNo1")
+        @remainSeat = data.xpath("//remainSeatCnt1")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   get 'home/index'
+  get '/station/huisu' => 'station#huisu'
   get '/station/:id' => 'station#getInfo'
 
   # The priority is based upon order of creation: first created -> highest priority.
